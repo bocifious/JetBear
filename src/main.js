902544481ec115dfadcbd90ddf763c718013728e
Patrick <pkbruso@gmail.com>
2020-10-02 15:42:49 -0700

    created splash screen

--- src/main.js
+++ src/main.js
@@ -1,11 +1,12 @@
 import Phaser from './lib/phaser.js'
 import Game from './scenes/Game.js'
+import Splash from './scenes/Splash.js'
 
 export default new Phaser.Game({
     type: Phaser.AUTO,
     width: 930,
     height: 360,
-    scene: Game,
+    scene: [Splash, Game],
     physics: {
         default: 'arcade',
         arcade: {
---
+++ src/scenes/Splash.js
@@ -0,0 +1,17 @@
+import Phaser from '../lib/phaser.js'
+
+export default class Splash extends Phaser.Scene {
+    constructor() {
+        super('splash');
+    }
+
+    preload() {
+        this.load.image('background-splash', 'assets/images/Splash.png');
+        this.load.image('start-button', 'assets/images/start.png');
+    }
+
+    create() {
+        this.add.image(465, 180, 'background-splash');
+        this.add.image(465, 200, 'start-button');
+    }
+}
