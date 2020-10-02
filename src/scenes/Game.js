064bc2f6dd5f87ba13e82c3d5412346ce632d5df
Patrick <pkbruso@gmail.com>
2020-10-02 15:10:10 -0700

    work on collider between foreground and player sprite

--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -6,12 +6,16 @@ export default class Game extends Phaser.Scene {
     }
 
     preload() {
-        this.load.image('background', 'assets/images/background_wide.png');
+        this.load.image('background', 'assets/images/bgtop.png');
+        this.load.image('foreground', 'assets/images/bgbottom.png');
         this.load.image('bear-off', 'assets/images/idle.png');
     }
 
     create() {
         this.add.image(465, 180, 'background');
-        this.physics.add.image(this.scale.width / 4, this.scale.height / 2, 'bear-off');
+        const foreground = this.add.image(465, 180, 'foreground'); //need to remove transparent part of foreground for collider to work
+        const player = this.physics.add.sprite(this.scale.width / 4, this.scale.height / 2, 'bear-off');
+
+        this.physics.add.collider(foreground, player);
     }
 }
