940ad4aa0afd803e866997872a997eab26076b4a
Patrick <pkbruso@gmail.com>
2020-10-02 16:09:23 -0700

    added intro music to splash screen

--- src/scenes/Splash.js
+++ src/scenes/Splash.js
@@ -8,13 +8,20 @@ export default class Splash extends Phaser.Scene {
     preload() {
         this.load.image('background-splash', 'assets/images/Splash.png');
         this.load.image('start-button', 'assets/images/start.png');
+        this.load.audio('intro', 'assets/audio/intro.mp3');
     }
 
     create() {
         this.add.image(465, 180, 'background-splash');
+
+        var music = this.sound.add('intro');
+        music.setLoop(true);
+        music.play();
+        
         var start = this.add.sprite(465, 250, 'start-button').setInteractive();
 
         start.on('pointerdown', function (event) {
+            music.stop();
             this.scene.start('game');
         }, this);
     }
