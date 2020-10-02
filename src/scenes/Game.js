ef64acda9b3de669ca416dae91ca61717b4b4c25
Patrick <pkbruso@gmail.com>
2020-10-02 14:57:30 -0700

    game starts with jetpack off sprite

--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -7,11 +7,11 @@ export default class Game extends Phaser.Scene {
 
     preload() {
         this.load.image('background', 'assets/images/background_wide.png');
-        this.load.image('bear', 'assets/images/bear.png');
+        this.load.image('bear-off', 'assets/images/idle.png');
     }
 
     create() {
         this.add.image(465, 180, 'background');
-        this.physics.add.image(this.scale.width / 4, this.scale.height / 2, 'bear');
+        this.physics.add.image(this.scale.width / 4, this.scale.height / 2, 'bear-off');
     }
 }
