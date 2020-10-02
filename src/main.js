967d27baddfe72668d49f3f648daba4db8741e17
Patrick <pkbruso@gmail.com>
2020-10-02 11:21:26 -0700

    load background

--- src/main.js
+++ src/main.js
@@ -1,2 +1,9 @@
 import Phaser from './lib/phaser.js'
+import Game from './scenes/Game.js'
 
+export default new Phaser.Game({
+    type: Phaser.AUTO,
+    width: 930,
+    height: 360,
+    scene: Game
+})
