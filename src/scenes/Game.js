82bbe9ba0da7d84c24d1a6899cfd232629200d66
Patrick <pkbruso@gmail.com>
2020-10-02 15:36:07 -0700

    removed foreground collider

--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -13,9 +13,9 @@ export default class Game extends Phaser.Scene {
 
     create() {
         this.add.image(465, 180, 'background');
-        const foreground = this.add.image(465, 180, 'foreground'); //need to remove transparent part of foreground for collider to work
-        const player = this.physics.add.sprite(this.scale.width / 4, this.scale.height / 2, 'bear-off');
+        this.add.image(465, 180, 'foreground');
+        this.physics.add.sprite(this.scale.width / 4, this.scale.height / 2, 'bear-off');
 
-        this.physics.add.collider(foreground, player);
+
     }
 }
