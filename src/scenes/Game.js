0f0bf8469186a85dd3d3ced50322b9bdaca2623f
Patrick <pkbruso@gmail.com>
2020-10-02 21:25:40 -0700

    completed logic to jetpack sprite when space pressed

--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -16,6 +16,7 @@ export default class Game extends Phaser.Scene {
         this.load.image('background', 'assets/images/bgtop.png');
         this.load.image('foreground', 'assets/images/bgbottom.png');
         this.load.image('bear-off', 'assets/images/idle.png');
+        this.load.image('bear-on', 'assets/images/bear.png');
 
         this.cursors = this.input.keyboard.createCursorKeys();
     }
@@ -29,12 +30,20 @@ export default class Game extends Phaser.Scene {
     update() {
         if (this.cursors.space.isDown) {
             this.bear.setVelocityY(-200);
+            this.bear.setTexture('bear-on');
         }
+
+        const vy = this.bear.body.velocity.y;
+        if (vy > 0 && this.bear.texture.key !== 'bear-off') {
+            this.bear.setTexture('bear-off');
+        }
+
+        this.groundStop(this.bear);
     }
 
-    groundStop(sprite) {
+    groundStop(sprite) { // see page 34
         const gameHeight = this.scale.height;
-        if (sprite.y >= gameHeight) {
+        if (sprite.y > gameHeight) {
             this.sprite.setVelocityY(0);
         }
     }
