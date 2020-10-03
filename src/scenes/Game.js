51b3b4a95fd378b2c5a7b840b651889473a89596
Patrick <pkbruso@gmail.com>
2020-10-02 21:14:39 -0700

    logic for jetpack up when space pressed

--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -5,6 +5,9 @@ export default class Game extends Phaser.Scene {
     /** @type {Phaser.Physics.Arcade.Sprite} */
     bear
 
+    /** @type {Phaser.Types.Input.Keyboard.CursorKeys} */
+    cursors
+
     constructor() {
         super('game');
     }
@@ -13,11 +16,26 @@ export default class Game extends Phaser.Scene {
         this.load.image('background', 'assets/images/bgtop.png');
         this.load.image('foreground', 'assets/images/bgbottom.png');
         this.load.image('bear-off', 'assets/images/idle.png');
+
+        this.cursors = this.input.keyboard.createCursorKeys();
     }
 
     create() {
         this.add.image(465, 180, 'background');
         this.add.image(465, 180, 'foreground');
         this.bear = this.physics.add.sprite(this.scale.width / 4, this.scale.height / 2, 'bear-off');
+    }
+
+    update() {
+        if (this.cursors.space.isDown) {
+            this.bear.setVelocityY(-200);
+        }
+    }
+
+    groundStop(sprite) {
+        const gameHeight = this.scale.height;
+        if (sprite.y >= gameHeight) {
+            this.sprite.setVelocityY(0);
+        }
     }
 }
